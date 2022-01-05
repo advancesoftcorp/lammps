f827e25fb3281aa2e704623cee279699be2618c7
Satomichi Nishihara <nishihara.satomichi@advance.jp>
2022-01-05 16:20:42 +0900

    fix typo

--- src/OC20DRIVER/oc20_driver.py
+++ src/OC20DRIVER/oc20_driver.py
@@ -5,7 +5,7 @@ This source code is licensed under the GNU General Public License Version 2
 found in the LICENSE file in the root directory of this source tree.
 """
 
-import touch
+import torch
 import yaml
 
 from ase import Atoms
